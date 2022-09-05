6b7b8e06ac277f1e8d070153b621954ec120472e
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-09-05 16:33:07 +0200

    installation de mangoose

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
+console.log("TEST3");
 
-app.use((req, res) => {
-  res.json({ message: "La requête  a bien été  bien reçue!" });
+
+
+
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Votre requête a bien été reçue !" });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
 });
 
 module.exports = app;
+
+
