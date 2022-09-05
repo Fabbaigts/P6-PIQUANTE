5ed83791b48621d890f5cdb5aa0b138af6c446b8
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-09-05 20:14:56 +0200

    Cors réglé, mais toujours erreur 500

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,20 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+//set header pour les cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 console.log("TEST3");
 
 app.use("/api/auth", userRoutes);
