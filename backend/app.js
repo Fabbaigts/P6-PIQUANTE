a53f6bf45244f06a130f45c6159b3de2bb5196f1
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-09-05 16:57:41 +0200

    creation base mongoDB+ identitfication depuis mangoose ok

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://fabien:P6-piquante@piquante.lumujnc.mongodb.net/test",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
 console.log("TEST3");
-
-
-
 
 app.use((req, res, next) => {
   console.log("Requête reçue !");
@@ -25,5 +32,3 @@ app.use((req, res, next) => {
 });
 
 module.exports = app;
-
-
