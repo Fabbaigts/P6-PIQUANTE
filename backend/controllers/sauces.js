f995e2c7eaf4897f6fb0349abb224bf8f0b7a2e1
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-09-12 14:18:11 +0200

    activation de la fonctionnalité de lecteur de toutes les sauces mais probleme d'affichage image

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -28,3 +28,16 @@ exports.createSauce = (req, res, next) => {
       });
     });
 };
+
+exports.getAllSauce = (req, res, next) => { sauce
+    .find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+    };
+    
+exports.getOneSauce = (req, res, next) => {
+      sauce
+        .findOne({ _id: req.params.id })
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+    };
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,10 +4,10 @@ const auth = require("../middleware/auth");
 const saucesCtrl = require("../controllers/sauces");
 const multer = require("../middleware/multer-config");
 
-//router.get("/", auth, saucesCtrl.getAllSauce);
-//router.get("/:id", auth, saucesCtrl.getOneSauce);
+router.get("/", auth, saucesCtrl.getAllSauce);
+router.get("/:id", auth, saucesCtrl.getOneSauce);
 router.post("/", auth, multer, saucesCtrl.createSauce);
 //router.put("/", auth, saucesCtrl.modifySauce);
-// router.delete("/", auth, saucesCtrl.deleteSauce);
+//router.delete("/", auth, saucesCtrl.deleteSauce);
 
 module.exports = router;
