b58745d71e74ec7a4319f91878c99aa46d72fa39
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-09-22 17:02:36 +0200

    Finalisation des likes et dislikes

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,7 +1,10 @@
 const sauce = require("../models/sauces");
-const fs = require("fs");
+const fs = require("fs"); // pour l'accès au système de fichiers
 
-//middleware pour la création d'une sauce
+// *****************************************************************************************
+// **********************  middleware pour la création d'une sauce *************************
+// *****************************************************************************************
+
 exports.createSauce = (req, res, next) => {
   const objetSauce = JSON.parse(req.body.sauce);
   console.log(objetSauce);
@@ -28,7 +31,11 @@ exports.createSauce = (req, res, next) => {
       });
     });
 };
-//Middleware pour la récupération de toutes les sauces de la base MongoDb
+
+// *****************************************************************************************
+// ******  Middleware pour la récupération de toutes les sauces de la base MongoDb *********
+// *****************************************************************************************
+
 exports.getAllSauce = (req, res, next) => {
   sauce
     .find()
@@ -36,8 +43,9 @@ exports.getAllSauce = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
   console.log("Affichage des sauces réussi !");
 };
-
-//Middleware pour la récupération d'une sauce en particulier de la base MongoDb
+// *****************************************************************************************
+// **** Middleware pour la récupération d'une sauce en particulier de la base MongoDb ******
+// *****************************************************************************************
 exports.getOneSauce = (req, res, next) => {
   console.log(req.params.id);
   sauce
@@ -45,10 +53,13 @@ exports.getOneSauce = (req, res, next) => {
     .then((sauce) => res.status(200).json(sauce))
     .catch((error) => res.status(404).json({ error }));
   console.log("Sauce trouvée !");
-  console.log(req.body.bearer);
+  console.log(req.rawHeaders[9]);
 };
 
-//Middleware pour la modification d'une sauces de la base MongoDb
+// *****************************************************************************************
+// ********** Middleware pour la modification d'une sauces de la base MongoDb **************
+// *****************************************************************************************
+
 exports.modifySauce = (req, res, next) => {
   const objetSauce = req.file
     ? {
@@ -58,6 +69,7 @@ exports.modifySauce = (req, res, next) => {
         }`,
       }
     : { ...req.body };
+  console.log(objetSauce);
 
   delete objetSauce._userId;
   sauce
@@ -88,17 +100,9 @@ exports.modifySauce = (req, res, next) => {
     });
 };
 
-/*exports.deleteSauce = (req, res, next) => {
-  console.log(req.params.id);
-  sauce
-    .deleteOne({ _id: req.params.id })
-    .then((sauce) => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(404).json({ error }));
-  console.log("Objet supprimé !");
-}
-*/
-
-//*************** Middleware pour la suppression d'une sauce de la base MongoDb et suppression image du serveur *****************
+// ******************************************************************************************************
+// *** Middleware pour la suppression d'une sauce de la base MongoDb et suppression image du serveur ****
+// ******************************************************************************************************
 exports.deleteSauce = (req, res, next) => {
   console.log(req.auth.userId);
 
@@ -125,9 +129,12 @@ exports.deleteSauce = (req, res, next) => {
     });
 };
 
+// *************************************************************************************************
 // ***************** Middleware pour la notation (like et dislike) des sauces **********************
+// *************************************************************************************************
+
 exports.like = (req, res, next) => {
-  //Cas de l'user qui clique sur like :
+  //*******************  Cas de l'user qui clique sur like :  ***************************
 
   if (req.body.like === 1) {
     console.log(req.body);
@@ -142,8 +149,8 @@ exports.like = (req, res, next) => {
       .then(() => res.status(200).json())
       .catch((error) => res.status(400).json({ error }));
   }
-  // Cas du User qui n'aime pas la sauce
-  if (req.body.like === -1) {
+  //*****************  Cas du User qui n'aime pas la sauce  ***********************
+  else if (req.body.like === -1) {
     console.log(req.body);
     sauce
       .updateOne(
@@ -156,24 +163,44 @@ exports.like = (req, res, next) => {
       .then(() => res.status(200).json())
       .catch((error) => res.status(400).json({ error }));
   }
-  // **************  Rectification des likes et dislikes ************
-  if (req.body.like === 0) {
+  // ****************   Rectification des likes et dislikes  *******************
+  else if (req.body.like === 0) {
     sauce
       .findOne({ _id: req.params.id })
       .then((sauce) => {
-        // Condition pour savoir si le tableau inclue déjà l'UserId
+        // Condition pour savoir si le tableau inclus déjà l'UserId du body de la request
         if (sauce.usersLiked.includes(req.body.userId)) {
+          //Alors on actualise l'entrée like et usersLiked
+          sauce
+            .updateOne(
+              {
+                $inc: { likes: -1 }, //décrémentation de -1 de MONGODB
+                $pull: { usersLiked: req.body.userId }, //Retrait du UserLiked ($pull)de MONGODB
+              },
+              console.log(
+                "Nous sommes rentrés dans la fonction de suppression pull"
+              )
+            )
+            .then(() => {
+              res.status(200).json({ message: "Like retiré" });
+            })
+            .catch((error) => res.status(400).json({ error }));
+          console.log("req.body.userId : " + req.body.userId);
+          console.log("tableau likes : " + sauce.usersLiked);
+        } else if (sauce.usersDisliked.includes(req.body.userId)) {
           //Alors on actulise l'entrée like et usersLiked
           sauce
             .updateOne(
-              { _id: req.params.id },
               {
-                $inc: { likes: req.body.like - 1 }, //décrémentation de -1
-                $pull: { usersLiked: req.body.userId }, //Retrait du UserLiked ($pull)
-              }
+                $inc: { dislikes: -1 }, //décrémentation de -1 du Dislike de MONGODB
+                $pull: { usersDisliked: req.body.userId }, //Retrait du UserDisliked ($pull) de MONGODB
+              },
+              console.log(
+                "Nous sommes rentrés dans la fonction de suppression pull"
+              )
             )
-            .then(() => {
-              res.status(200).json();
+            .then((sauce) => {
+              res.status(200).json({ message: "dislike retiré" });
             })
             .catch((error) => res.status(400).json({ error }));
           console.log("req.body.userId : " + req.body.userId);
