6f851c5b051e347fef19cbe0185d2f5ad58cc609
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-09-30 22:28:14 +0200

    finalisation sel

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,17 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+// chiffrage email
 const crypto = require("crypto-js");
+// appel du module dotenv
 require("dotenv").config();
 
-// chiffrage email
+
+
+ let salt = bcrypt.genSaltSync(10);
+ console.log ("valeur du salt : " + salt);
+
+
 
 exports.signup = (req, res, next) => {
   console.log(req.body.password);
@@ -12,12 +19,13 @@ exports.signup = (req, res, next) => {
   const cryptoEmail = crypto
     .HmacSHA256(req.body.email, process.env.EMAIL_ENV)
     .toString();
-  console.log(cryptoEmail);
+ console.log("ceci est l'email crypté " + cryptoEmail);
+
   bcrypt
-    .hash(req.body.password, 10)
+    .hash(req.body.password, salt)
     .then((hash) => {
-      console.log(hash);
-      console.log(cryptoEmail);
+      console.log("ceci est le hash + sel  : " + hash);
+     
       const user = new User({
         email: cryptoEmail,
         password: hash,
@@ -43,23 +51,26 @@ exports.login = (req, res, next) => {
           .status(401)
           .json({ message: "Paire login/mot de passe incorrecte" });
       }
+
       bcrypt
         .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res
-              .status(401)
-              .json({ message: "Paire login/mot de passe incorrecte" });
-          }
+        .then((valid) => { 
+            if (!valid) {  
+              return res
+                .status(401)
+                .json({ message: "Paire login/mot de passe incorrecte" });
+            } 
+          
 
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign({ userId: user._id }, process.env.TOKEN_ENV, {
-              expiresIn: "24h",
-            }),
-          });
-        })
-        .catch((error) => res.status(500).json({ error }));
+            res.status(200).json({
+              userId: user._id,
+              token: jwt.sign({ userId: user._id }, process.env.TOKEN_ENV, {
+                expiresIn: "24h",
+              }),
+
+            });
+          })
+          .catch((error) => res.status(500).json({ error })); 
     })
     .catch((error) => res.status(500).json({ error }));
 };
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
-require ("dotenv").config();
+require("dotenv").config();
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.TOKEN_ENV);
     const userId = decodedToken.userId;
-    req.auth = { 
+    req.auth = {
       userId: userId,
     };
     next();
